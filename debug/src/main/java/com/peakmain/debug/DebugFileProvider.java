2c91efa1eecee7bbda3873dfc32a2a3f692a5368
peakmain <2726449200@qq.com>
2022-04-27 17:46:40 +0800

    分享bug修复

---
+++ debug/src/main/java/com/peakmain/debug/DebugFileProvider.java
@@ -0,0 +1,12 @@
+package com.peakmain.debug;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * author ：Peakmain
+ * createTime：2022/4/27
+ * mail:2726449200@qq.com
+ * describe：
+ */
+public class DebugFileProvider extends FileProvider {
+}
